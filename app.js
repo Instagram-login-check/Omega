5f6ef897904b47d639a75150595d93e94588f6bd
Instagram-login-check <69810751+Instagram-login-check@users.noreply.github.com>
2020-08-17 18:43:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAbTGt0S5aFF3HICsf3OVWheirolwj02Ss",
+  authDomain: "instagram-login-check.firebaseapp.com",
+  databaseURL: "https://instagram-login-check.firebaseio.com",
+  projectId: "instagram-login-check",
+  storageBucket: "instagram-login-check.appspot.com",
+  messagingSenderId: "509611360513",
+  appId: "1:509611360513:web:8850de4ab3870246e9db00",
+  measurementId: "G-F3SBCSTSFB"
 };
 
      
